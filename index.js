6829b1269994591303cb3e26952a16f0b6328a19
GReyes4 <105297867+GReyes4@users.noreply.github.com>
2024-04-04 10:21:57 -0600

    Eliminar últimos cambios (para merge automatico)

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import express from 'express';
 // Import App routes
 import usuarios from './controllers/usuarios.js';
 import reservaciones from './controllers/reservaciones.js';
-import experiencias from './controllers/experiencias.js';
 
 const port = process.env.PORT || 3000;
 
@@ -11,7 +10,6 @@ const app = express();
 
 app.use('/usuarios', usuarios);
 app.use('/reservaciones', reservaciones);
-app.use('/experiencias', experiencias);
 
 // Start the server
 app.listen(port, () => {
