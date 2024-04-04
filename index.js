20571213300791909e8ab81b89430af00f5a5a02
GReyes4 <105297867+GReyes4@users.noreply.github.com>
2024-04-04 10:14:05 -0600

    Metodo base de experiencias
    Traer todas las experiencias

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 // Import App routes
 import usuarios from './controllers/usuarios.js';
 import reservaciones from './controllers/reservaciones.js';
+import experiencias from './controllers/experiencias.js';
 
 const port = process.env.PORT || 3000;
 
@@ -10,6 +11,7 @@ const app = express();
 
 app.use('/usuarios', usuarios);
 app.use('/reservaciones', reservaciones);
+app.use('/experiencias', experiencias);
 
 // Start the server
 app.listen(port, () => {
