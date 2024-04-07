011f129129796fbf52d5c02a8499c901a799ce3c
Jinellie <89491839+Jinellie@users.noreply.github.com>
2024-04-06 21:24:15 -0600

    Update Setup.sql

--- queries/Setup.sql
+++ queries/Setup.sql
@@ -73,7 +73,8 @@ CREATE TABLE Salas (
     idSala INT PRIMARY KEY,
     nombre VARCHAR(255),
     descripcion VARCHAR(255),
-    cantidadMesas INT
+    cantidadMesas INT,
+    fotoURL VARCHAR(255)
 );
 
 CREATE TABLE Experiencias (
@@ -172,10 +173,19 @@ INSERT INTO Materiales (idMaterial, nombre, fotoURL) VALUES
 (2, 'Material 2', 'url_material_2');
 
 -- Sample data for Salas
-INSERT INTO Salas (idSala, nombre, descripcion, cantidadMesas) VALUES
-(1, 'Sala VR', 'Ideal para experimentar con lentes de realidad virtual ya sean experiencias o videojuegos ', 5),
-(2, 'Electric garage', 'Ideal para temas de electronica ', 10),
-(3, 'Deep net', 'Ideal para experimentar con temas de redes y ciberseguridad', 5);
+INSERT INTO Salas (idSala, nombre, descripcion, cantidadMesas, fotoURL) VALUES
+(1, 'Electric Garage', 'Este espacio dinámico y versátil es un sueño hecho realidad para los entusiastas de la electrónica. Equipado con las últimas herramientas y tecnologías, es el lugar ideal para dar vida a tus proyectos más ambiciosos.', 8, 'url_electric_garage'),
+(2, 'Dimension Forge', 'Un laboratorio de vanguardia donde la creatividad se fusiona con la tecnología. Aquí, los innovadores pueden explorar libremente nuevas ideas y experimentar con las últimas herramientas de diseño y fabricación.', 6, 'https://dreamlabstorage.blob.core.windows.net/archivos/vr-lede.jpg'),
+(3, 'New Horizons', 'Inspirado por la curiosidad y el deseo de explorar lo desconocido, New Horizons es un lugar donde los límites de la tecnología se desdibujan. Desde la inteligencia artificial hasta la exploración espacial, aquí se dan los primeros pasos hacia el futuro.', 7, 'url_new_horizons'),
+(4, 'Deep Net', 'Sumérgete en las profundidades de la seguridad informática y las redes con Deep Net. Equipado con tecnología de última generación y expertos en el campo, es el lugar perfecto para poner a prueba tus habilidades y descubrir nuevos horizontes en el ciberespacio.', 5, 'url_deep_net'),
+(5, 'Graveyard', 'No es un lugar de descanso, sino de reinvención. Graveyard es donde las ideas obsoletas encuentran una nueva vida y las tecnologías pasadas se transforman en innovaciones futuras. Es el punto de partida para los visionarios y los revolucionarios.', 9, 'url_graveyard'),
+(6, 'PCB Factory', 'Desde prototipos hasta producción en masa, PCB Factory ofrece un entorno especializado para el diseño y la fabricación de placas de circuito impreso. Con equipos de alta precisión y experiencia técnica, cada proyecto encuentra su camino hacia el éxito.', 10, 'url_pcb_factory'),
+(7, 'Hack-Battlefield', 'Adéntrate en un campo de pruebas donde la habilidad y la estrategia son tus armas. Hack-Battlefield es el lugar donde los expertos en seguridad informática se enfrentan para poner a prueba sus habilidades y proteger los sistemas de mañana.', 6, 'url_hack_battlefield'),
+(8, 'Testing Land', 'Un terreno fértil para la innovación y el desarrollo tecnológico. Aquí, los proyectos toman forma y se someten a rigurosas pruebas para garantizar su calidad y fiabilidad. Es el punto de partida para las soluciones del futuro.', 8, 'url_testing_land'),
+(9, 'War Headquarters', 'El corazón estratégico de las operaciones tecnológicas avanzadas. War Headquarters es donde se planifican y ejecutan los proyectos más ambiciosos, donde la creatividad se encuentra con la ingeniería para dar forma al futuro de la tecnología.', 5, 'url_war_headquarters'),
+(10, 'Biometrics Flexible Hall', 'En un mundo donde la identidad es fundamental, Biometrics Flexible Hall ofrece un entorno adaptable para la investigación y el desarrollo de sistemas biométricos. Desde el reconocimiento facial hasta la autenticación de voz, aquí se están construyendo las soluciones de seguridad del mañana.', 7, 'url_biometrics_flexible_hall'),
+(11, 'Beyond-Digits', 'Más allá de los límites convencionales de la tecnología, Beyond-Digits es donde las ideas audaces encuentran su hogar. Aquí, los innovadores exploran nuevas fronteras, desde la inteligencia artificial hasta la computación cuántica, dando forma al futuro con cada línea de código.', 9, 'url_beyond_digits');
+
 
 -- Sample data for Experiencias
 INSERT INTO Experiencias (idExperiencia, idUF, idSala, nombre, descripcion, esAutoDirigida, portadaURL, fechaInicio, fechaFin, horaFin) VALUES
