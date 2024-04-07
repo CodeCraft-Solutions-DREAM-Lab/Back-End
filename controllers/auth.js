b79212d8b9e3b15f2c54a25259bcc173add2e0f8
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-06 22:01:59 -0600

    Cambiar BD para pruebas y funcion para autenticar jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,18 @@
 import sha512 from "js-sha512";
 import { config } from "../config.js";
 import Database from "../database.js";
+import jwt from "jsonwebtoken";
 
 // Create database object
 const database = new Database(config);
+const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
 export async function authUsuario(req, res, next) {
   try {
     const { usuario, contrasena } = req.body;
     const shaPasword = sha512(String(contrasena));
+
+    console.log(usuario, contrasena, shaPasword);
 
     const result = await database.readAndConditions(
       "Credenciales",
@@ -16,13 +20,19 @@ export async function authUsuario(req, res, next) {
         { idName: "idUsuario", id: usuario },
         { idName: "contrasena", id: shaPasword },
       ],
-      "token"
+      "idUsuario"
     );
 
+    console.log(result);
+
     if (result.length === 0) {
-      res.status(404).json(result);
+      res.status(404).json({});
     } else {
-      res.status(200).json(result);
+      var token = jwt.sign({ usuario: usuario }, TOKEN_SECRET, {
+        expiresIn: "7d",
+      });
+      console.log("Token: ", token);
+      res.status(200).json({ jwt: token });
     }
   } catch (err) {
     res.status(500).json({});
@@ -32,16 +42,9 @@ export async function authUsuario(req, res, next) {
 export async function authToken(req, res, next) {
   const { token } = req.body;
 
-  console.log(token);
-
   try {
-    const result = await database.readStringId("Credenciales", "token", token);
-
-    if (result.length === 0) {
-      res.status(404).json(false);
-    } else {
-      res.status(200).json(true);
-    }
+    var decoded = jwt.verify(token, TOKEN_SECRET);
+    res.status(200).json(true);
   } catch (err) {
     res.status(500).json(false);
   }
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from "express";
 import { router } from "./routes/routes.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.development" });
 
 const port = process.env.PORT || 3000;
 
@@ -10,6 +14,9 @@ app.use(express.json());
 // Para escribir datos en el body al hacer post o put
 // Si llegaramos a necesitar objetos mas complejos, poner extended en true
 app.use(express.urlencoded({ extended: false }));
+
+// Para poder hacer peticiones desde cualquier origen
+app.use(cors());
 
 app.use(router);
 
