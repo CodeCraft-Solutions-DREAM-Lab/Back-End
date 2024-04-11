ffc7669628d387ee1599b8cd4d12bbdb35dc312d
GReyes4 <105297867+GReyes4@users.noreply.github.com>
2024-04-11 08:42:36 -0600

    Add GetById de salas y experiencias
    - Tmb agregué los stop procedures a azure

--- controllers/experiencias.js
+++ controllers/experiencias.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.use(express.json());
 // Create database object
 const database = new Database(config);
+
 router.get('/', async (_, res) => {
     try {
         // Regresa todas las experiencias
@@ -15,5 +16,23 @@ router.get('/', async (_, res) => {
         res.status(500).json({ error: err?.message });
     }
 });
+
+router.get("/:id", async (req, res) => {
+    try {
+      const experienciaId = req.params.id;
+      console.log(`experienciaId: ${experienciaId}`);
+      if (experienciaId) {
+        const result = await database.executeQuery(
+          `EXEC [dbo].[getExperienciaById] @idExperiencia = ${experienciaId};`
+        );
+        console.log(`experiencia: ${JSON.stringify(result)}`);
+        res.status(200).json(result.recordset);
+      } else {
+        res.status(404);
+      }
+    } catch (err) {
+      res.status(500).json({ error: err?.message });
+    }
+  });
 
 export default router;
--- controllers/salas.js
+++ controllers/salas.js
@@ -16,4 +16,23 @@ router.get('/', async (_, res) => {
     }
 });
 
+router.get("/:id", async (req, res) => {
+    try {
+      const salaId = req.params.id;
+      console.log(`salaId: ${salaId}`);
+      if (salaId) {
+        const result = await database.executeQuery(
+          `EXEC [dbo].[getSalaById] @idSala = ${salaId};`
+        );
+        console.log(`sala: ${JSON.stringify(result)}`);
+        res.status(200).json(result.recordset);
+      } else {
+        res.status(404);
+      }
+    } catch (err) {
+      res.status(500).json({ error: err?.message });
+    }
+  });
+
 export default router;
+
