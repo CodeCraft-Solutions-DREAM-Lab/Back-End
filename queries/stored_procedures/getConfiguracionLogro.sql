7dad48eaa5a6a34ba474089cca4c1c9703ca2e61
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-29 11:49:45 -0600

    Agregar SP para los logros

---
+++ queries/stored_procedures/getConfiguracionLogro.sql
@@ -0,0 +1,17 @@
+DROP PROCEDURE IF EXISTS getConfiguracionLogro;  
+GO  
+
+CREATE PROCEDURE getConfiguracionLogro 
+    @idUsuario VARCHAR(10)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT l.nombre, l.iconoURL, u.colorPreferido
+    	FROM Logros AS l
+		INNER JOIN UsuariosLogros ul ON l.idLogro = ul.idLogro
+        INNER JOIN Usuarios u ON ul.idUsuario = u.idUsuario
+	WHERE
+		ul.idUsuario = @idUsuario AND
+		u.logroPrincipal = ul.idLogro
+END
