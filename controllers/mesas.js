d35cdec5786aa80285d9e0b2674664b3150f6ff7
Christopher Pedraza <gabalixreborn@gmail.com>
2024-05-20 22:01:03 -0600

    Actualizar docs mesas

--- controllers/mesas.js
+++ controllers/mesas.js
@@ -8,40 +8,43 @@ router.use(express.json());
 // Create database object
 const database = new Database(config);
 
-/**
- * @openapi
- * /mesas:
- *  get:
- *    summary: Obtiene todas las mesas
- *    tags:
- *     - Mesas
- *    responses:
- *      200:
- *        description: OK
- *        content:
- *          application/json:
- *            schema:
- *              type: array
- *              items:
- *                type: object
- *                properties:
- *                  idMesa:
- *                    type: integer
- *                  idSala:
- *                    type: integer
- *                  cupos:
- *                    type: integer
- *      500:
- *        description: Error
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                error:
- *                  type: string
- */
 router.get("/", async (_, res) => {
+    /*
+    #swagger.tags = ['Mesas']
+    #swagger.description = 'Obtiene todas las mesas'
+    #swagger.summary = 'Obtiene todas las mesas'
+    #swagger.responses[200] = {
+        description: 'OK',
+        content: {
+            'application/json': {
+                schema: {
+                    type: 'array',
+                    items: {
+                        type: 'object',
+                        properties: {
+                            idMesa: { type: 'integer' },
+                            idSala: { type: 'integer' },
+                            cupos: { type: 'integer' }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    #swagger.responses[500] = {
+        description: 'Error',
+        content: {
+            'application/json': {
+                schema: {
+                    type: 'object',
+                    properties: {
+                        error: { type: 'string' }
+                    }
+                }
+            }
+        }
+    }
+    */
     try {
         // Return a list of usuarios
         const usuarios = await database.readAll("Mesas");
@@ -52,41 +55,44 @@ router.get("/", async (_, res) => {
     }
 });
 
-/**
- * @openapi
- * /mesas/{idSala}:
- *  get:
- *    summary: Obtiene las mesas de una sala específica
- *    tags:
- *     - Mesas
- *    parameters:
- *      - in: path
- *        name: idSala
- *        schema:
- *          type: integer
- *        required: true
- *        description: ID de la sala
- *    responses:
- *      200:
- *        description: OK
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                maxCupos:
- *                  type: integer
- *      500:
- *        description: Error
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                error:
- *                  type: string
- */
 router.get("/:idSala", async (req, res) => {
+    /*
+    #swagger.tags = ['Mesas']
+    #swagger.description = 'Obtiene las mesas de una sala específica'
+    #swagger.summary = 'Obtiene las mesas de una sala específica'
+    #swagger.parameters['idSala'] = {
+        in: 'path',
+        description: 'ID de la sala',
+        required: true,
+        type: 'integer'
+    }
+    #swagger.responses[200] = {
+        description: 'OK',
+        content: {
+            'application/json': {
+                schema: {
+                    type: 'object',
+                    properties: {
+                        maxCupos: { type: 'integer' }
+                    }
+                }
+            }
+        }
+    }
+    #swagger.responses[500] = {
+        description: 'Error',
+        content: {
+            'application/json': {
+                schema: {
+                    type: 'object',
+                    properties: {
+                        error: { type: 'string' }
+                    }
+                }
+            }
+        }
+    }
+    */
     try {
         const salaId = req.params.idSala;
 
