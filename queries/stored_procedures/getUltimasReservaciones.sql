180ada673a4e6b8aa285e8d61e676dc3c87f499b
GReyes4 <105297867+GReyes4@users.noreply.github.com>
2024-05-27 00:16:44 -0600

    Add experiencias populares y cambio estetico

--- queries/stored_procedures/getUltimasReservaciones.sql
+++ queries/stored_procedures/getUltimasReservaciones.sql
@@ -12,30 +12,76 @@ BEGIN
         FROM [dbo].[Reservaciones] r
         WHERE r.idUsuario = @idUsuario
         ORDER BY r.fecha DESC
+    ),
+    SalasUnicas AS (
+        SELECT DISTINCT 
+            null AS idExperiencia, 
+            s.idSala, 
+            s.nombre, 
+            s.fotoURL AS URL, 
+            'sala' AS tipo
+        FROM UltimasReservaciones ur
+        JOIN [dbo].[Salas] s ON ur.idSala = s.idSala
+    ),
+    ExperienciasUnicas AS (
+        SELECT DISTINCT 
+            e.idExperiencia, 
+            e.idSala, 
+            e.nombre, 
+            e.portadaURL AS URL, 
+            'experiencia' AS tipo
+        FROM UltimasReservaciones ur
+        JOIN [dbo].[Experiencias] e ON ur.idExperiencia = e.idExperiencia
+        WHERE ur.idExperiencia IS NOT NULL
+    ),
+    ExperienciasUFs AS (
+        SELECT 
+            E.idExperiencia, 
+            E.idSala, 
+            E.nombre, 
+            E.portadaURL AS URL, 
+            'experiencia' AS tipo
+        FROM Experiencias E
+        WHERE E.idUF IN (
+            SELECT DISTINCT GU.idUF
+            FROM GruposUsuarios GU
+            WHERE GU.idUsuario = @idUsuario
+        )
+    ),
+    ExperienciasPopulares AS (
+        SELECT TOP 6
+            E.idExperiencia,
+            E.idSala,
+            E.nombre,
+            E.portadaURL AS URL,
+            'experiencia' AS tipo,
+            COUNT(R.idReservacion) AS cantidadReservas
+        FROM 
+            Experiencias E
+        JOIN 
+            Reservaciones R ON E.idExperiencia = R.idExperiencia
+        GROUP BY  
+            E.idExperiencia, E.idSala, E.nombre, E.portadaURL
+        ORDER BY 
+            COUNT(R.idReservacion) DESC
     )
 
-	-- Seleccionar salas únicas
-    SELECT DISTINCT null AS 'idExperiencia', s.idSala, s.nombre, s.fotoURL AS 'URL', 'sala' AS tipo
-    FROM UltimasReservaciones ur
-    JOIN [dbo].[Salas] s ON ur.idSala = s.idSala
+    -- Union de todas las consultas
+    SELECT idExperiencia, idSala, nombre, URL, tipo FROM SalasUnicas
 
-	UNION
+    UNION
 
-    -- Seleccionar experiencias únicas
-    SELECT DISTINCT e.idExperiencia, e.idSala, e.nombre, e.portadaURL AS 'URL', 'experiencia' AS tipo
-    FROM UltimasReservaciones ur
-    JOIN [dbo].[Experiencias] e ON ur.idExperiencia = e.idExperiencia
-    WHERE ur.idExperiencia IS NOT NULL 
+    SELECT idExperiencia, idSala, nombre, URL, tipo FROM ExperienciasUnicas
 
-	UNION 
+    UNION
 
-	-- Seleccionar Experiencias UFs
-	SELECT E.idExperiencia, E.idSala, E.nombre, E.portadaURL AS 'URL', 'experiencia' AS tipo
-    FROM Experiencias E
-    WHERE E.idUF IN (
-        SELECT DISTINCT GU.idUF
-        FROM GruposUsuarios GU
-        WHERE GU.idUsuario = 'a00833852'
-    );
+    SELECT idExperiencia, idSala, nombre, URL, tipo FROM ExperienciasUFs
+
+    UNION
+
+    SELECT idExperiencia, idSala, nombre, URL, tipo FROM (
+        SELECT idExperiencia, idSala, nombre, URL, tipo
+        FROM ExperienciasPopulares
+    ) AS Populares;
 
 END;
