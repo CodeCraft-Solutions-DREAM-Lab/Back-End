e98a664ffa105566267006d4cfe5cccb264cb2fc
JaimedevSwirvle <jaime@swirvle.com>
2024-06-03 10:11:57 -0600

    Ya se traen los datos para el modal

--- queries/stored_procedures/getReservacionesConfirmadasCronograma.sql
+++ queries/stored_procedures/getReservacionesConfirmadasCronograma.sql
@@ -4,6 +4,7 @@ BEGIN
     SELECT 
         R.idReservacion AS id,
         R.idMesa AS [group],
+        R.estatus AS estatus,
         CONCAT(U.nombre, ' ', U.apellidoP, ' ', U.apellidoM) AS title,
         CAST(CONCAT(CONVERT(VARCHAR, R.fecha, 23), ' ', CONVERT(VARCHAR, R.horaInicio, 8)) AS DATETIME) AS start_time,
         DATEADD(HOUR, R.duracion, CAST(CONCAT(CONVERT(VARCHAR, R.fecha, 23), ' ', CONVERT(VARCHAR, R.horaInicio, 8)) AS DATETIME)) AS end_time
@@ -12,5 +13,5 @@ BEGIN
     INNER JOIN 
         Usuarios U ON R.idUsuario = U.idUsuario
     WHERE 
-        R.estatus = 3;
+        R.estatus = 3 OR R.estatus = 2 OR R.estatus = 1 OR R.estatus = 7;
 END;
