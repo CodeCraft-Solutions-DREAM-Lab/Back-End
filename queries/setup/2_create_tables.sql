9ee9a9c773c6ed0f40118a7f1094ec986f25b3f9
Christopher Pedraza <gabalixreborn@gmail.com>
2024-06-05 17:15:00 -0600

    Dividir setup

--- queries/setup/2_create_tables.sql
+++ queries/setup/2_create_tables.sql
@@ -1,37 +1,4 @@
--- DROP ALL TABLES IF THEY EXIST
-DROP TABLE IF EXISTS MaterialesRecomendados;
-
-DROP TABLE IF EXISTS MaterialesSalas;
-
-DROP TABLE IF EXISTS ReservacionesMateriales;
-
-DROP TABLE IF EXISTS Reservaciones;
-
-DROP TABLE IF EXISTS Experiencias;
-
-DROP TABLE IF EXISTS Mesas;
-
-DROP TABLE IF EXISTS Salas;
-
-DROP TABLE IF EXISTS Materiales;
-
-DROP TABLE IF EXISTS UsuariosLogros;
-
-DROP TABLE IF EXISTS Logros;
-
-DROP TABLE IF EXISTS HistorialPrioridad;
-
-DROP TABLE IF EXISTS GruposUsuarios;
-
-DROP TABLE IF EXISTS Credenciales;
-
-DROP TABLE IF EXISTS Usuarios;
-
-DROP TABLE IF EXISTS UnidadesFormacion;
-
-DROP TABLE IF EXISTS Estatus;
-
--- CREATE THE TABLES
+-- Creates the tables for the database
 CREATE TABLE
     UnidadesFormacion (
         idUF INT PRIMARY KEY IDENTITY (1, 1),
