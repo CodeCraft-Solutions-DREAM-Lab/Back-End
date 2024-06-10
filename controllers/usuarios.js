5f3dec76705c4d53b7f19464a59f4e0dd2ba5a1c
Christopher Pedraza <gabalixreborn@gmail.com>
2024-06-10 12:08:32 -0600

    Actualizar usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -11,7 +11,7 @@ router.use(express.json());
 const database = new Database(config);
 
 router.get("/", async (_, res) => {
-	/*
+    /*
     #swagger.tags = ['Usuarios']
     #swagger.description = 'Obtiene todos los usuarios'
     #swagger.summary = 'Obtiene todos los usuarios'
@@ -29,7 +29,9 @@ router.get("/", async (_, res) => {
                             apellidoP: { type: 'string' },
                             apellidoM: { type: 'string' },
                             tipo: { type: 'string' },
-                            prioridad: { type: 'integer' }
+                            prioridad: { type: 'integer' },
+                            logroPrincipal: { type: 'integer' },
+                            colorPreferido: { type: 'string' }
                         }
                     }
                 }
@@ -50,21 +52,22 @@ router.get("/", async (_, res) => {
         }
     }
     */
-	try {
-		// Return a list of usuarios
-		const usuarios = await database.readAll("Usuarios");
-		console.log(`Usuarios: ${JSON.stringify(usuarios)}`);
-		res.status(200).json(usuarios);
-	} catch (err) {
-		res.status(500).json({ error: err?.message });
-	}
+    try {
+        // Return a list of usuarios
+        const usuarios = await database.readAll("Usuarios");
+        console.log(`Usuarios: ${JSON.stringify(usuarios)}`);
+        res.status(200).json(usuarios);
+    } catch (err) {
+        res.status(500).json({ error: err?.message });
+    }
 });
 
 router.put("/:idUsuario", async (req, res) => {
-	/*
+    /*
     #swagger.tags = ['Usuarios']
     #swagger.description = 'Obtiene un usuario por id'
     #swagger.summary = 'Obtiene un usuario por id'
+    #swagger.deprecated = true
     #swagger.parameters['idUsuario'] = {
         in: 'path',
         description: 'id del usuario',
@@ -105,21 +108,21 @@ router.put("/:idUsuario", async (req, res) => {
         }
     }
     */
-	try {
-		const usuarioId = req.params.idUsuario;
-		console.log(`usuarioId: ${usuarioId}`);
-		const valor = req.body;
+    try {
+        const usuarioId = req.params.idUsuario;
+        console.log(`usuarioId: ${usuarioId}`);
+        const valor = req.body;
 
-		const rowsAffected = await database.update(
-			"Usuarios",
-			"idUsuario",
-			usuarioId,
-			valor
-		);
-		res.status(200).json({ rowsAffected });
-	} catch (err) {
-		res.status(500).json({ error: err?.message });
-	}
+        const rowsAffected = await database.update(
+            "Usuarios",
+            "idUsuario",
+            usuarioId,
+            valor
+        );
+        res.status(200).json({ rowsAffected });
+    } catch (err) {
+        res.status(500).json({ error: err?.message });
+    }
 });
 
 router.get("/nombreUsuario/:tagID", async (req, res) => {
@@ -128,30 +131,28 @@ router.get("/nombreUsuario/:tagID", async (req, res) => {
     #swagger.description = 'Obtiene el nombre de usuario asociado a un tag ID específico.'
     #swagger.summary = 'Obtener nombre de usuario por Tag ID'
     #swagger.parameters['tagID'] = {
-
         in: 'path',
         description: 'ID del tag del usuario',
         required: true,
         type: 'string'
     }
-
     #swagger.responses[200] = {
-
         description: 'OK',
         content: {
             'application/json': {
                 schema: {
-                    type: 'object',
-                    properties: {
-                        nombreUsuario: { type: 'string', example: 'John Doe' }
+                    type: 'array',
+                    items: {
+                        type: 'object',
+                        properties: {
+                            nombreUsuario: { type: 'string' }
+                        }
                     }
                 }
             }
         }
     }
-
     #swagger.responses[500] = {
-
         description: 'Error',
         content: {
             'application/json': {
@@ -183,7 +184,7 @@ router.get("/nombreUsuario/:tagID", async (req, res) => {
 });
 
 router.post("/cambiarPrioridad", async (req, res) => {
-	/*
+    /*
     #swagger.tags = ['Usuarios']
     #swagger.description = 'Cambia los puntos de prioridad de un usuario, lo guarda en el historial de cambios y envía un correo al usuario'
     #swagger.summary = 'Cambia los puntos de prioridad de un usuario'
@@ -242,58 +243,55 @@ router.post("/cambiarPrioridad", async (req, res) => {
         }
     }
     */
-	try {
-		const { idUsuario, puntos, motivo } = req.body;
+    try {
+        const { idUsuario, puntos, motivo } = req.body;
 
-		if (!idUsuario || !puntos || !motivo) {
-			res.status(400).json({
-				error: "idUsuario, puntos and motivo are required",
-			});
-			return;
-		}
+        if (!idUsuario || !puntos || !motivo) {
+            res.status(400).json({
+                error: "idUsuario, puntos and motivo are required",
+            });
+            return;
+        }
 
-		await database.executeProcedure("addPrioridadToUser", {
-			idUsuario,
-			prioridad: puntos,
-		});
+        await database.executeProcedure("addPrioridadToUser", {
+            idUsuario,
+            prioridad: puntos,
+        });
 
-		const currentDate = new Date();
-		const sqlDate = currentDate.toISOString().split("T")[0];
+        const currentDate = new Date();
+        const sqlDate = currentDate.toISOString().split("T")[0];
 
-		await database.executeProcedure("insertIntoHistorialPrioridad", {
-			idUsuario,
-			fecha: sqlDate,
-			motivo,
-			prioridad: puntos,
-		});
+        await database.executeProcedure("insertIntoHistorialPrioridad", {
+            idUsuario,
+            fecha: sqlDate,
+            motivo,
+            prioridad: puntos,
+        });
 
-		let mensaje = "";
+        let mensaje = "";
 
-		if (puntos >= 0) {
-			mensaje = "Tu prioridad ha aumentado por " + puntos + " puntos.";
-		} else {
-			mensaje = "Tu prioridad ha disminuido por " + puntos + " puntos.";
-		}
+        if (puntos >= 0) {
+            mensaje = "Tu prioridad ha aumentado por " + puntos + " puntos.";
+        } else {
+            mensaje = "Tu prioridad ha disminuido por " + puntos + " puntos.";
+        }
 
-		const htmlTemplate = getHtmlTemplate(
-			"updatedPriorityPoints",
-			{
-				mensaje: mensaje,
-				motivo: motivo,
-			}
-		);
+        const htmlTemplate = getHtmlTemplate("updatedPriorityPoints", {
+            mensaje: mensaje,
+            motivo: motivo,
+        });
 
-		sendEmail(
-			`${idUsuario.toUpperCase()}@tec.mx`,
-			"Prioridad actualizada",
-			"",
-			htmlTemplate
-		);
+        sendEmail(
+            `${idUsuario.toUpperCase()}@tec.mx`,
+            "Prioridad actualizada",
+            "",
+            htmlTemplate
+        );
 
-		return res.status(200).json({ message: "Prioridad updated" });
-	} catch (err) {
-		res.status(500).json({ error: err?.message });
-	}
+        return res.status(200).json({ message: "Prioridad updated" });
+    } catch (err) {
+        res.status(500).json({ error: err?.message });
+    }
 });
 
 export default router;
