959fd765e5e255de5b293dd7d6b643a5f2ebedf2
Christopher Pedraza <gabalixreborn@gmail.com>
2024-06-11 23:28:23 -0600

    Documentar

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,6 @@
 import express from "express";
+
+// Controladores
 import usuarios from "../controllers/usuarios.js";
 import reservaciones from "../controllers/reservaciones.js";
 import salas from "../controllers/salas.js";
@@ -11,16 +13,13 @@ import perfil from "../controllers/perfil.js";
 import logros from "../controllers/logros.js";
 import dashboard from "../controllers/dashboard.js";
 import reservacionesMateriales from "../controllers/reservacionesMateriales.js";
-
 import correrAsignacion from "../controllers/correrAsignacion.js";
-import sendReminder2hrsBefore from "../controllers/schedules/sendReminder2hrsBefore.js";
 import estatus from "../controllers/estatus.js";
 import ufs from "../controllers/ufs.js";
 
-import { getHtmlTemplate, sendEmail } from "../emails/nodemailer.js";
-
 const router = express.Router();
 
+// Rutas
 router.use("/usuarios", usuarios);
 router.use("/auth", auth);
 router.use("/reservaciones", reservaciones);
@@ -36,36 +35,5 @@ router.use("/dashboard", dashboard);
 router.use("/correr-asignacion", correrAsignacion);
 router.use("/estatus", estatus);
 router.use("/ufs", ufs);
-
-
-
-router.get("/test", async () => {
-    await sendReminder2hrsBefore();
-});
-
-router.get("/test-email", async (
-    req,
-    res
-) => {
-
-    const emailHtml = getHtmlTemplate(
-        "reservReqDenied",
-        {
-            nombre_usuario: "Efraín",
-            logro: "Epic Dreamer",
-            sala: "",
-            fecha: "15 de Enero",
-            horaInicio: "10:00",
-            horaFin: "12:00",
-        }
-    );
-
-    await sendEmail(
-        `A01280601@tec.mx`,
-        "Haz obtenido un nuevo logro",
-        "",
-        emailHtml
-    );
-});
 
 export { router };
